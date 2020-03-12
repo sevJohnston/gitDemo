e7e8eb20632515bf4bdfef43f1bfcaa7053e0945
sevJohnston <severena@comcast.net>
2020-03-12 12:08:03 -0700

    big upload

---
+++ HikeDemo/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HikeDemo.Models
+{
+    public class AppUser : IdentityUser
+    {
+        //IdentityUser class contains Id, UserName, Clains, Email, etc
+        //-- so you don't need to keep them here
+    }
+}
---
+++ HikeDemo/Models/Hike.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HikeDemo.Models
+{
+    public class Hike
+    {
+        private List<Hike> hikes = new List<Hike>();
+        //not sure I'm adding reviews
+        //private List<UserReview> reviews = new List<UserReview>();
+        public int HikeID { get; set; }     //creates a HikeID and maps it as the primary key
+        public string TrailName { get; set; }
+        public string Region { get; set; }
+        public string Description { get; set; }
+
+        public List<Hike> Hikes { get { return hikes; } }
+        //public List<UserReview> UserReviews { get { return reviews; } }
+    }
+}
